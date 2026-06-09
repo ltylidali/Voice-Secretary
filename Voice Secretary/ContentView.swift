70c6232c36966fef03341a32e12f00a4c62ca1d5
Tianyu Li <ltylidali@hotmail.com>
2026-06-08 20:23:32 -0700

    Initial Commit

---
+++ Voice Secretary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Voice Secretary
+//
+//  Created by tianyu li on 2026-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
